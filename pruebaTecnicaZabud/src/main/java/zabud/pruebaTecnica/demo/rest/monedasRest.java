b2dc5168b91d87506c51be8348632256b93bfcc2
CARLOS ALBERTO <CARLOS ALBERTO@LAPTOP-G189QO1L>
2020-12-17 15:30:17 -0500

    API Rest completa

--- pruebaTecnicaZabud/src/main/java/zabud/pruebaTecnica/demo/rest/monedasRest.java
+++ pruebaTecnicaZabud/src/main/java/zabud/pruebaTecnica/demo/rest/monedasRest.java
@@ -7,15 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import zabud.pruebaTecnica.demo.entity.moneda;
 import zabud.pruebaTecnica.demo.dao.monedaDAO;
+
 @RestController
 @RequestMapping("Alcancía")
 public class monedasRest {
-	
+
 	@Autowired
 	private monedaDAO monedaDAO;
 	
@@ -27,12 +30,23 @@ public class monedasRest {
 	@RequestMapping(value="{MonedaID}")
 	public ResponseEntity<moneda> getMonedaByID(@PathVariable("MonedaID") Long monedaID){
 		Optional<moneda> optionalMoneda = monedaDAO.findById(monedaID);
-		System.out.print(optionalMoneda.get());
 		if(optionalMoneda.isPresent()) {
 			return ResponseEntity.ok(optionalMoneda.get());
 		}else {
 			return ResponseEntity.noContent().build();
 		}
 	}
-		
+	@PutMapping
+	public ResponseEntity<moneda> updateMoneda(@RequestBody moneda alcancia){
+		Optional<moneda> optionalMoneda = monedaDAO.findById(alcancia.getId());
+		if (optionalMoneda.isPresent()) {
+			moneda updateMoneda = optionalMoneda.get();
+			updateMoneda.setCantidad(alcancia.getCantidad());
+			monedaDAO.save(updateMoneda);
+			return ResponseEntity.ok(updateMoneda);
+		}else {
+			return ResponseEntity.noContent().build();
+		}
+	}
+	
 }
