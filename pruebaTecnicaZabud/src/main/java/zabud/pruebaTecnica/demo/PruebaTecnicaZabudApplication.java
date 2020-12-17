f8277cbb438d319b868d6d72d9ab7c7bc2c40e6e
CARLOS ALBERTO <CARLOS ALBERTO@LAPTOP-G189QO1L>
2020-12-17 08:49:55 -0500

    Primer commit

---
+++ pruebaTecnicaZabud/src/main/java/zabud/pruebaTecnica/demo/PruebaTecnicaZabudApplication.java
@@ -0,0 +1,13 @@
+package zabud.pruebaTecnica.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaZabudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaZabudApplication.class, args);
+	}
+
+}
---
+++ pruebaTecnicaZabud/src/test/java/zabud/pruebaTecnica/demo/PruebaTecnicaZabudApplicationTests.java
@@ -0,0 +1,13 @@
+package zabud.pruebaTecnica.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaZabudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
